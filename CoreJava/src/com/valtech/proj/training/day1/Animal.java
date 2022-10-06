95773d709ee19d0358f6f50c93826af4e7f91c0b
Santhosh Kumar A <71362132+Santhosh1204@users.noreply.github.com>
2022-10-06 15:23:59 +0530

    Delete CoreJava/src/com/valtech/proj/training/day1 directory

--- CoreJava/src/com/valtech/proj/training/day1/Animal.java
+++
@@ -1,6 +0,0 @@
-package com.valtech.proj.training.day1;
-
-public interface Animal {
-	int NUM_OF_LEGS=4;
-	void makeSound();
-}
--- CoreJava/src/com/valtech/proj/training/day1/Hello.java
+++
@@ -1,9 +0,0 @@
-package com.valtech.proj.training.day1;
-
-
-public class Hello{
-public static void main(String[] args) {
-	System.out.println("Hello World");
-}
-
-}
